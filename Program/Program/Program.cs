8053c82e15b035dcaad5a62f61aa64ecd1b13202
Harmatter <kotej02@mail.ru>
2021-10-21 22:22:37 +0300

    Добавление классов пользователей.

---
+++ Program/Program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Napas
+{
+    class User
+    {
+        string Name;
+        int ID;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
