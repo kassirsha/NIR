5c4b8472d58c5eef3b936768286012200581dcf7
kassirsha <stevesbevenson@gmail.com>
2021-12-07 13:47:05 +0300

    Добавлены тесты для маппингов

---
+++ Program/Store.DataAccess.Tests/CommMapTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Store.DataAccess.Tests
+{
+    using FluentNHibernate.Testing;
+    using Store.Core;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Тесты на правила отображения <see cref="Library.DataAccess.Mappings.BookMap"/>.
+    /// </summary>
+    [TestFixture]
+    internal class CommMapTests : BaseMapTests
+    {
+        [Test]
+        public void PersistenceSpecification_ValidData_Success()
+        {
+            // arrange
+            var commodity = new Commodity(1, "Книга", 0);
+
+            // act & assert
+            new PersistenceSpecification<Commodity>(this.Session)
+                .VerifyTheMappings(commodity);
+        }
+    }
+}
